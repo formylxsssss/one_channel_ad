47facf34d986df5367efa8316bd578a1a76dd774
xuan <hua@gmail.com>
2026-08-13 09:38:26 +0800

    修改为海豚公网ip

--- device/app_net_config.h
+++ device/app_net_config.h
@@ -85,19 +85,19 @@ extern "C" {
  * 你的局域网测试中，电脑运行服务器脚本，所以填电脑 IP：192.168.19.16。
  */
 #ifndef APP_DEFAULT_SERVER_IP_A
-#define APP_DEFAULT_SERVER_IP_A         192U
+#define APP_DEFAULT_SERVER_IP_A         49U
 #endif
 
 #ifndef APP_DEFAULT_SERVER_IP_B
-#define APP_DEFAULT_SERVER_IP_B         168U
+#define APP_DEFAULT_SERVER_IP_B         239U
 #endif
 
 #ifndef APP_DEFAULT_SERVER_IP_C
-#define APP_DEFAULT_SERVER_IP_C         10U
+#define APP_DEFAULT_SERVER_IP_C         193U
 #endif
 
 #ifndef APP_DEFAULT_SERVER_IP_D
-#define APP_DEFAULT_SERVER_IP_D         2U
+#define APP_DEFAULT_SERVER_IP_D         149U
 #endif
 
 #ifndef APP_DEFAULT_SERVER_PORT
@@ -105,7 +105,7 @@ extern "C" {
 #endif
 
 #ifndef APP_DEFAULT_DEVICE_ID
-#define APP_DEFAULT_DEVICE_ID           1U
+#define APP_DEFAULT_DEVICE_ID           3U
 #endif
 
 #ifndef APP_TCP_RECONNECT_INTERVAL_MS
